ec12bdc108b5465f32cb51fe9bb3481b3d1cb124
اسم المستخدم الخاص بك < >
2026-03-05 09:10:03 +0300

    إضافة نسخة ثابتة مؤقتة للتطبيق

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/SpeedFusion-pro',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
